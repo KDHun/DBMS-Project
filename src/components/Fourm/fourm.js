da429e093413875d5fba809eb217c106dc5f0d58
Karabhai <mylove.as.ak47@gmail.com>
2021-11-20 10:25:21 +0530

    V5

--- src/components/Fourm/fourm.js
+++ src/components/Fourm/fourm.js
@@ -4,14 +4,17 @@ function Fourm(props) {
     return(
         <div className= {f.div1}>
             <Card className={'text-center' + 'f.card'}>
-            <Card.Header>Fourm</Card.Header>
+            <Card.Header className = {f.header1}>Fourm</Card.Header>
             <Card.Body>
                 <Card.Title>Question ID :{props.Qid}</Card.Title>
                 <Card.Text>
                 {props.Q}
                 </Card.Text>
+                <Card.Text>
+                    Date : {props.date} Time : {props.time}
+                </Card.Text>
             </Card.Body>
-            <Card.Footer className={'f.cfoot' + 'text-muted'}>Date : {props.date} Time : {props.time}</Card.Footer>
+            {/* <Card.Footer className={'f.cfoot' + 'text-muted'}>Date : {props.date} Time : {props.time}</Card.Footer> */}
             </Card>
         </div>
     );
